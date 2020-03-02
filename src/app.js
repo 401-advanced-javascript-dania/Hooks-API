21b9b418e6804a8ea0d00c3fe1cce690b9346644
daniaalrababa9 <rababadania@gmail.com>
2020-03-02 02:44:45 +0200

    i still didnot finish it

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './component/header.js';
+import Footer from './component/footer.js';
+import ToDoList from './component/toDoList.js'
+function App(){
+    return(
+        <React.Fragment>
+            <Header/>
+            <ToDoList/>
+            <Footer/>
+        </React.Fragment>
+    )
+}
+export default App;
---
+++ src/component/toDoList.js
@@ -0,0 +1,56 @@
+import React,{useEffect,useState} from 'react';
+function ToDoList(){
+    const [list,setList]=useState([]);
+    const [item,setItem]=useState('');
+    const [pepole,setPeople]=useState([]);
+    const [name,setName]=useState('');
+    const [StatusOfList,setStatusForList]=useState(false);
+    
+    const _changeItemText = (event) =>{
+        setItem(event.target.value);
+    }
+    const _changeName = (event) =>{
+        setName(event.target.value);
+    }
+    const _changeStatus=()=>{
+        
+        setStatusForList(list.length%2===0)  
+    }
+    const _addItem = (event) => {
+        event.preventDefault();
+        event.target.reset();
+        // setStatusForList(list.length>0)
+        item && setList([...list,item]);
+    }
+    const _addperson = (event) => {
+        event.preventDefault();
+        event.target.reset();
+        name && setPeople([...pepole,name]);
+    }
+    useEffect(()=>{
+console.log('item added');
+if(list.length>=1){document.title = `Status of list,${StatusOfList} `}
+    },[list])
+
+    return(
+        <section>
+         <form onSubmit={_addItem}>
+            <label>To Do Item Text</label>
+            <input onChange={_changeItemText}/><br></br>
+         <form onSubmit={_addperson}>
+         <label>Assigned To</label>
+         <input onChange={_changeName}/>
+         </form>
+    <p>Status Of List {StatusOfList.toString()}</p>
+            <button onClick={_changeStatus}></button>
+         </form>
+         {list.map(item=>
+             <li key={item}>{item}</li>
+         )}
+           {pepole.map(item=>
+             <p key={item}>{item}</p>
+         )}
+        </section>
+    )
+}
+export default ToDoList;
